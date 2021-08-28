eb396a56dd2516023bed08a0f53fc2751aba4220
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-08-28 15:56:30 -0400

    filter in place

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Post from "./components/Post/Post";
 
 function App() {
+  // 1. Hard code your UI.
+  // 2. Hard code data, and set the UI to use that data.
+  // 3. Dynamically get my data.
+
+  const [posts, setPosts] = useState([]);
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        // console.log(response.data);
+        setPosts(response.data);
+        const filteredPosts = response.data.filter(post => post.userId === 1);
+        setMyPosts(filteredPosts);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h1>All Posts</h1>
+          {posts.map((post) => (
+            <Post {...post} key={post.id} />
+          ))}
+        </div>
+        <div className="col-6">
+          <h1>My Posts</h1>
+          {myPosts.map((post) => (
+            <Post {...post} key={post.id} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
