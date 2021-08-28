e3e264930da6a1afa463e26d41be63e9c3f6c52a
Jonathan J. Watson <jonathanwatson1@gmail.com>
2021-08-28 17:02:13 -0400

    actions, reducers, and constants, oh my

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { GET_ARTICLES } from "../constants/action-types";
 
 export function getArticles() {
   return function (dispatch) {
     return axios
       .get("https://jsonplaceholder.typicode.com/posts")
       .then((response) => {
-        dispatch({ type: "GET_ARTICLES", payload: response.data });
+        dispatch({ type: GET_ARTICLES, payload: response.data });
       });
   };
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
+import { GET_ARTICLES } from "../constants/action-types";
+
 const initialState = {
   articles: [],
 };
 
 function rootReducer(state = initialState, action) {
-  if (action.type === "GET_ARTICLES") {
+  if (action.type === GET_ARTICLES) {
     // console.log(action.payload);
     return Object.assign({}, state, { articles: action.payload });
   }
